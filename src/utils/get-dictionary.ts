8890b1986880d1ab741adefd24986f2034478a92
kimizuy <kimizuyamasaki@gmail.com>
2023-11-11 16:15:19 -0800

    add get-dictionary

---
+++ src/utils/get-dictionary.ts
@@ -0,0 +1,11 @@
+import "server-only";
+import type { Locale } from "./type";
+
+const dictionaries = {
+  en: () => import("../dictionaries/en.json").then((module) => module.default),
+  de: () => import("../dictionaries/de.json").then((module) => module.default),
+  ja: () => import("../dictionaries/ja.json").then((module) => module.default),
+};
+
+export const getDictionary = async (locale: Locale) =>
+  dictionaries[locale]?.() ?? dictionaries.en();
