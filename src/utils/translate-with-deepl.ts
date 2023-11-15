ca6b48df2f1896b16df427273d63158534bc9c92
kimizuy <kimizuyamasaki@gmail.com>
2023-11-14 16:42:25 -0800

    Revert "debug: remove react-cache"
    This reverts commit 780e40c08da779a0ba4992e834bd27c43b880515.

--- src/utils/translate-with-deepl.ts
+++ src/utils/translate-with-deepl.ts
@@ -3,20 +3,23 @@ import "server-only";
 import * as deepl from "deepl-node";
 import { Locale } from "./types";
 import { i18nConfig } from "./i18n-config";
+import { cache } from "react";
 
 const translator = new deepl.Translator(process.env.DEEPL_API_KEY!);
 
-export const translateWithDeepL = async (text: any, targetLang: Locale) => {
-  if (targetLang === i18nConfig.defaultLocale) return text;
-  if (typeof text !== "string") return text;
+export const translateWithDeepL = cache(
+  async (text: any, targetLang: Locale) => {
+    if (targetLang === i18nConfig.defaultLocale) return text;
+    if (typeof text !== "string") return text;
 
-  const translated = await translator.translateText(
-    text,
-    i18nConfig.defaultLocale,
-    targetLang === "en" ? "en-US" : targetLang,
-    // "context" is alpha feature. it may be deprecated in the future
-    // ref: https://github.com/DeepLcom/deepl-node#text-translation-options
-    { context: "Web Developer Portfolio Sites" }
-  );
-  return translated.text;
-};
+    const translated = await translator.translateText(
+      text,
+      i18nConfig.defaultLocale,
+      targetLang === "en" ? "en-US" : targetLang,
+      // "context" is alpha feature. it may be deprecated in the future
+      // ref: https://github.com/DeepLcom/deepl-node#text-translation-options
+      { context: "Web Developer Portfolio Sites" }
+    );
+    return translated.text;
+  }
+);
