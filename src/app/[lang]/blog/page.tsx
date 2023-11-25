477ff60f345c1394c3cf8776c6e789a55b246f31
kimizuy <kimizuyamasaki@gmail.com>
2023-11-24 16:58:16 -0800

    update style

--- src/app/[lang]/blog/page.tsx
+++ src/app/[lang]/blog/page.tsx
@@ -21,7 +21,7 @@ export default async function Page({ params: { lang } }: Props) {
     <div>
       <ul className="grid gap-4">
         {postMatters.map((matter) => (
-          <li key={matter.slug} className="flex gap-2">
+          <li key={matter.slug} className="flex flex-wrap gap-2">
             <time dateTime={matter.publishedAt.toISOString()}>
               {format(matter.publishedAt, "yyyy-MM-dd")}
             </time>
