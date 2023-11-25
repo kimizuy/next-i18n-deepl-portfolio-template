196e8e51683fe9a5e1540e67016baf0feea3ccac
kimizuy <kimizuyamasaki@gmail.com>
2023-11-24 16:56:37 -0800

    add note for translate

--- src/dictionaries/de.ts
+++ src/dictionaries/de.ts
@@ -3,4 +3,6 @@ import { Dictionary } from "./ja";
 export const de: Dictionary = {
   siteDescription: "Standortbeschreibung",
   backToBlogTop: "Zurück zum Anfang des Blog",
+  language: "Sprache",
+  note: `Diese Seite wurde von <a href='https://www.deepl.com/' target="_blank" rel="noopener noreferrer">Deepl</a> übersetzt.`,
 };
