d17afd19ee350b180d147ba6ccca3742d7ea30ca
kimizuy <kimizuyamasaki@gmail.com>
2023-11-30 03:18:34 -0800

    add test

--- src/utils/valibot.ts
+++ src/utils/valibot.ts
@@ -5,6 +5,31 @@ const FrontmatterSchema = object({
   publishedAt: date(),
 });
 
-export const isFrontmatter = (
+export function isFrontmatter(
   value: unknown
-): value is Output<typeof FrontmatterSchema> => is(FrontmatterSchema, value);
+): value is Output<typeof FrontmatterSchema> {
+  return is(FrontmatterSchema, value);
+}
+
+if (import.meta.vitest) {
+  const { describe, it, expect } = import.meta.vitest;
+  describe("isFrontmatter returns true when the value is a frontmatter", () => {
+    it("returns true", () => {
+      const frontmatter = {
+        title: "titl",
+        publishedAt: new Date(),
+      };
+      expect(isFrontmatter(frontmatter)).toBe(true);
+    });
+  });
+
+  describe("isFrontmatter returns false when the value is not a frontmatter", () => {
+    it("returns false", () => {
+      const frontmatter = {
+        title: "titl",
+        publishedAt: "2021-08-20",
+      };
+      expect(isFrontmatter(frontmatter)).toBe(false);
+    });
+  });
+}
